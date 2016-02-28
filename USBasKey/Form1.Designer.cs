b9d2b7e7d73ce83855a9c577e76f99417a6c35ce
PhillyAI <phillyai314@gmail.com>
2016-02-28 15:39:25 +0900

    final

--- USBasKey/Form1.Designer.cs
+++ USBasKey/Form1.Designer.cs
@@ -29,11 +29,63 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans CJK JP Regular", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(579, 99);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USB 보안 프로그램";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Noto Sans CJK JP Regular", 30F);
+            this.label2.Location = new System.Drawing.Point(37, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 59);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(1005, 431);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Noto Sans CJK JP Regular", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- USBasKey/Form1.cs
+++ USBasKey/Form1.cs
@@ -9,12 +9,40 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
 using System.IO;
 
 namespace USBasKey
 {
     public partial class Form1 : Form
     {
+        Size fullScreen;
+        
+        [DllImport("user32.dll")]
+        private static extern int SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(int hhk);
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(int hhk, int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        public struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        private const int WM_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int WM_SYSKEYUP = 0x0105;
+        private delegate int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+        private static int _hookID = 0;
+        private static LowLevelKeyboardProc _proc = HookCallback;
         string key = "";
         public Form1()
         {
@@ -22,12 +50,47 @@ namespace USBasKey
 
             foreach(string s in File.ReadAllLines(Environment.CurrentDirectory + "\\Settings.txt"))
             {
-                if (s.StartsWith("\\")) continue;
+                if (s.StartsWith("//")) continue;
                 key += s;
             }
-            
+
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                _hookID = SetWindowsHookEx(WM_KEYBOARD_LL, _proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+            SetTaskManager(false);
+            FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Location = new Point(0, 0);
+            this.Size = new Size(10000, 10000);
+            if(!File.Exists(Environment.CurrentDirectory + "\\Crypt.dll"))
+            {
+                StreamWriter sr = new StreamWriter(Environment.CurrentDirectory + "\\Crypt.dll");
+                sr.Write(key);
+                sr.Flush();
+                sr.Dispose();
+            }
         }
 
+        void Exit()
+        {
+            SetTaskManager(true);
+            UnhookWindowsHookEx(_hookID);
+            Application.Exit();
+
+        }
+        private static int HookCallback(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam)
+        {
+            if (nCode >= 0)
+            {
+                if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN || wParam == WM_KEYUP || wParam == WM_SYSKEYUP)
+                {
+                    return 1;
+                }
+            }
+            return CallNextHookEx(0, nCode, wParam, ref lParam);
+        }
         protected override void WndProc(ref Message m)
         {
             UInt32 WM_DEVICECHANGE = 0x0219;
@@ -58,6 +121,17 @@ namespace USBasKey
             base.WndProc(ref m);
         }
 
+        public void SetTaskManager(bool enable)
+        {
+            RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(
+                @"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if (enable && objRegistryKey.GetValue("DisableTaskMgr") != null)
+                objRegistryKey.DeleteValue("DisableTaskMgr");
+            else
+                objRegistryKey.SetValue("DisableTaskMgr", "1");
+            objRegistryKey.Close();
+        }
+
         void Evented()
         {
             foreach(string device in System.IO.Directory.GetLogicalDrives())
@@ -65,7 +139,7 @@ namespace USBasKey
                 System.IO.DriveInfo dr = new System.IO.DriveInfo(device);
                 if (dr.DriveType != System.IO.DriveType.Removable) continue;
                 if (Check(device))
-                    Application.Exit();
+                    Exit();
             }
         }
 
@@ -75,15 +149,25 @@ namespace USBasKey
             if (!System.IO.File.Exists(fullname))
                 return false;
             FileInfo f = new FileInfo(fullname);
-            if (f.Attributes != FileAttributes.Hidden)
+            if ((f.Attributes | FileAttributes.Hidden) != f.Attributes)
                 return false;
 
-
-            foreach(string s in File.ReadAllLines(deviceDirect))
+            //File.SetAttributes(deviceDirect, FileAttributes.Normal);
+            foreach(string s in File.ReadAllLines(fullname))
             {
                 if (s.StartsWith(key)) return true;
             }
             return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Evented();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label2.Text = string.Format("현재 시간 : {0}", DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss tt"));
         }
     }
 }
