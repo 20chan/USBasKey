7042ed3cd1bdaea2ccbb147d70c403bd47b6bc0f
PhillyAI <phillyai314@gmail.com>
2016-02-29 21:33:18 +0900

    update
    made keymaker.

---
+++ KeyMaker/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace KeyMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.이름 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.경로 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.이름,
+            this.경로});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 15);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(301, 195);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // 이름
+            // 
+            this.이름.Text = "이름";
+            this.이름.Width = 180;
+            // 
+            // 경로
+            // 
+            this.경로.Text = "경로";
+            this.경로.Width = 99;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "선택한 USB에 쓰기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 223);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "USB 에 쓰기";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 이름;
+        private System.Windows.Forms.ColumnHeader 경로;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ KeyMaker/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace KeyMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Evented();
+        }
+
+        public void MakeCryptFile(string directory)
+        {
+            try
+            {
+                string key = "";
+                foreach (string s in File.ReadAllLines(Environment.CurrentDirectory + "\\Settings.txt"))
+                {
+                    if (s.StartsWith("//")) continue;
+                    key += s;
+                }
+                if (File.Exists(directory + "\\Crypt.dll"))
+                {
+                    File.SetAttributes(directory + "\\Crypt.dll", FileAttributes.Normal);
+                }
+                StreamWriter sr = new StreamWriter(directory + "\\Crypt.dll");
+                sr.Write(key);
+                sr.Flush();
+                sr.Dispose();
+
+                File.SetAttributes(directory + "\\Crypt.dll", FileAttributes.Hidden);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public List<DriveInfo> USBDrives()
+        {
+            List<DriveInfo> usbs = new List<DriveInfo>();
+            foreach (string device in System.IO.Directory.GetLogicalDrives())
+            {
+                System.IO.DriveInfo dr = new System.IO.DriveInfo(device);
+                if (dr.DriveType != System.IO.DriveType.Removable) continue;
+                usbs.Add(dr);
+            }
+            return usbs;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("선택하신 드라이브가 없습니다!");
+                return;
+            }
+            MakeCryptFile(this.listView1.SelectedItems[0].SubItems[1].Text);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            UInt32 WM_DEVICECHANGE = 0x0219;
+            UInt32 DBT_DEVTUP_VOLUME = 0x02;
+            UInt32 DBT_DEVICEARRIVAL = 0x8000;
+            UInt32 DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+            if ((m.Msg == WM_DEVICECHANGE) && (m.WParam.ToInt32() == DBT_DEVICEARRIVAL))//디바이스 연결
+            {
+                //int m_Count = 0;
+                int devType = Marshal.ReadInt32(m.LParam, 4);
+
+                if (devType == DBT_DEVTUP_VOLUME)
+                {
+                    Evented();
+                }
+            }
+
+            if ((m.Msg == WM_DEVICECHANGE) && (m.WParam.ToInt32() == DBT_DEVICEREMOVECOMPLETE))  //디바이스 연결 해제
+            {
+                int devType = Marshal.ReadInt32(m.LParam, 4);
+                if (devType == DBT_DEVTUP_VOLUME)
+                {
+                    Evented();
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void Evented()
+        {
+            List<DriveInfo> Usbs = USBDrives();
+            foreach(DriveInfo d in Usbs)
+            {
+                ListViewItem i = new ListViewItem(d.VolumeLabel);
+                i.SubItems.Add(d.RootDirectory.ToString());
+                this.listView1.Items.Add(i);
+            }
+        }
+    }
+}
